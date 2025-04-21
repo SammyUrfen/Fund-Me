b2647578c56de4402a52754ce6838ebedbe56c36
SammyUrfen <bibekcharah@gmail.com>
2025-04-21 18:57:11 +0530

    environment commit for hosting

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,16 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyD7byvBsNY--rSqmu4pwvP8TR2frDVv3XU",
-    authDomain: "fund-me-183c8.firebaseapp.com",
-    projectId: "fund-me-183c8",
-    storageBucket: "fund-me-183c8.firebasestorage.app",
-    messagingSenderId: "994743483211",
-    appId: "1:994743483211:web:0abf310b27c42b8c7bc6ce"  
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 
